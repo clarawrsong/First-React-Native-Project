d999cd016a6726be0f84eb6dec566ce175e519f2
Clara Song <claRAWRsong@gmail.com>
2018-08-08 22:56:20 -0400

    Update Input.js

--- components/Input.js
+++ components/Input.js
@@ -9,7 +9,7 @@ const styles = StyleSheet.create({
   },
 });
 
-export default class Title extends Component {
+export default class Input extends Component {
   render() {
     return (
       <View style={styles.input}>
